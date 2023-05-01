338c2b48c98a562e5afa49c8f6c895ee8645df22
famo1245 <kandallee38@gmail.com>
2023-05-01 11:51:04 +0900

    chap 3

--- README.md
+++
@@ -1,2 +0,0 @@
-# Algorithm_with_Python
-파이썬을 이용한 코딩 테스트 공부
--- chap3/q2.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-scores = list(map(int, input().split()))
-
-if len(scores) != n:
-    print('입력한 과목의 수와 다릅니다')
-
-else:
-    M = max(scores)
-    print(sum(scores) / M * 100 / n)
