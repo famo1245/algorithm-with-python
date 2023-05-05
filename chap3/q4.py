e4a87c4b57e951615aab355c4c963baf0545a03c
famo1245 <kandallee38@gmail.com>
2023-05-05 22:16:08 +0900

    solved q4 in chap 3

--- chap3/q4.py
+++ chap3/q4.py
@@ -1,22 +1,19 @@
 import sys
 input = sys.stdin.readline
-n = int(input())
+n, m = map(int, input().split())
 A = [[0] * (n+1)]
-D = [[0] * (n+1)] * (n+1)
+# D = [[0] * (n+1)] * (n+1) # *를 이용해 배열 선언 하면 얕은 복사, 배열 내의 요소들이 같은 객체를 가리킴
+D = [[0] * (n+1) for _ in range(n+1)]
 
 for i in range(n):
     A_row = [0] + [int(x) for x in input().split()]
     A.append(A_row)
 
-print(A)
-
 for i in range(1, n+1):
     for j in range(1, n+1):
         D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j]
 
-print(D)
-#
-# for _ in range(m):
-#     x1, y1, x2, y2 = map(int, input().split())
-#     answer = D[x2][y2] - (D[x1-1][y2] + D[x2][y1-1] + D[x1-1][y1-1])
-#     print(answer)
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    answer = D[x2][y2] - (D[x1-1][y2] + D[x2][y1-1]) + D[x1-1][y1-1]
+    print(answer)
