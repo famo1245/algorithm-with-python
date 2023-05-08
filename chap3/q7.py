4f40a88939bc402a8803d28645f9e8d56a8a1a2e
famo1245 <kandallee38@gmail.com>
2023-05-08 12:06:16 +0900

    solved q7 in chap3

--- chap3/q7.py
+++ chap3/q7.py
@@ -7,8 +7,20 @@ G = list(map(int, input().split()))
 G.sort()
 
 start_idx = 1
-end_idx = 1
-count = 1
-sum = 1
+end_idx = N - 1
+count = 0
 
-# while end_idx != N:
+while start_idx < end_idx:
+    sum = G[start_idx] + G[end_idx]
+    if sum == M:
+        count += 1
+        start_idx += 1
+        end_idx -= 1
+
+    elif sum > M:
+        end_idx -= 1
+
+    else:
+        start_idx += 1
+
+print(count)
