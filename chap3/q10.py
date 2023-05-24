36bdc4926206e3e086c69e061e5f9ad1bfa887af
famo1245 <kandallee38@gmail.com>
2023-05-24 18:05:18 +0900

    solved q10 in chap3

--- chap3/q10.py
+++ chap3/q10.py
@@ -1,19 +1,34 @@
-import sys
+# import sys
+#
+# input = sys.stdin.readline
+#
+# N, L = map(int, input().split())
+# A = list(map(int, input().split()))
+# temp = list()
+# answer = list()
+#
+# for i in range(N):
+#     temp.append(A[i])
+#
+#     if len(temp) == L + 1:
+#         temp.pop(0)
+#
+#     answer.append(min(temp))
+#
+# for i in answer:
+#     print(i, end=' ')
+# 내 풀이 -> 시간 초과
 
-input = sys.stdin.readline
+from collections import deque
 
 N, L = map(int, input().split())
-A = list()
-temp = list()
-answer = list()
+mydeque = deque()
+now = list(map(int, input().split()))
 
 for i in range(N):
-    temp.append(A[i])
-
-    if len(temp) == L + 1:
-        temp.pop(0)
-
-    answer.append(min(temp))
-
-for i in answer:
-    print(i, end=' ')
+    while mydeque and mydeque[-1][0] > now[i]:
+        mydeque.pop()
+    mydeque.append((now[i], i))
+    if mydeque[0][1] <= i - L:
+        mydeque.popleft()
+    print(mydeque[0][0], end=' ')
