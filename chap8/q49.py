cf0e78dd245cb5903f9a6114a8a94d256e3285db
JIYOUNG KIM <kandallee38@gmail.com>
2024-03-29 00:57:54 +0900

    solving q49 in chap 8

---
+++ chap8/q49.py
@@ -0,0 +1,11 @@
+# BOJ 1717
+n, m = map(int, input().split())
+L = [i for i in range(n + 1)]
+
+for _ in range(m):
+    command, a, b = map(int, input().split())
+
+    if command == 0:
+        continue
+    elif command == 1:
+        continue
