ba220aa31c99f6b5aaab0e59b6daf374c5102a40
JIYOUNG KIM <kandallee38@gmail.com>
2024-05-11 21:19:16 +0900

    solved q58 in chap 8

--- chap8/q58.py
+++ chap8/q58.py
@@ -51,14 +51,48 @@
 #     else:
 #         print(time[k - 1])
 
+# import sys
+# import math
+# from queue import PriorityQueue
+#
+# input = sys.stdin.readline
+#
+# n, m, k = map(int, input().split())
+# routes = [[] for _ in range(n + 1)]
+# times = [[math.inf] * k for _ in range(n + 1)]
+#
+# for _ in range(m):
+#     a, b, c = map(int, input().split())
+#     routes[a].append((b, c))
+#
+# start = 1
+# times[start][0] = 0
+# que = PriorityQueue()
+# que.put((times[start][0], start))
+#
+# while not que.empty():
+#     now_time, now = que.get()
+#
+#     for node, value in routes[now]:
+#         times[node].sort()
+#         if times[node][k - 1] > now_time + value:
+#             times[node][k - 1] = now_time + value
+#             que.put((times[node][k - 1], node))
+#
+# for i in range(1, len(times)):
+#     time = times[i]
+#     if time[k - 1] == math.inf:
+#         print(-1)
+#     else:
+#         print(time[k - 1])
+
 import sys
 import math
-from queue import PriorityQueue
+import heapq
 
 input = sys.stdin.readline
 
 n, m, k = map(int, input().split())
-# visited = [False] * (n + 1)
 routes = [[] for _ in range(n + 1)]
 times = [[math.inf] * k for _ in range(n + 1)]
 
@@ -68,22 +102,18 @@ for _ in range(m):
 
 start = 1
 times[start][0] = 0
-que = PriorityQueue()
-que.put((times[start][0], start))
+que = []
+heapq.heapify(que)
+heapq.heappush(que, (times[start][0], start))
 
-while not que.empty():
-    now_time, now = que.get()
-    # if visited[now]:
-    #     continue
-    # visited[now] = True
+while que:
+    now_time, now = heapq.heappop(que)
 
     for node, value in routes[now]:
-        times[node].sort()
         if times[node][k - 1] > now_time + value:
             times[node][k - 1] = now_time + value
-            # if not visited[node]:
-            #     que.put((times[node][k - 1], node))
-            que.put((times[node][k - 1], node))
+            heapq.heappush(que, (times[node][k - 1], node))
+            times[node].sort()
 
 for i in range(1, len(times)):
     time = times[i]
