5f0c2ed9720154d2c7dcfe865512237607e7c0ac
JIYOUNG KIM <kandallee38@gmail.com>
2024-06-09 02:24:53 +0900

    solving q81 in chap 9

--- chap10/q81.py
+++ chap10/q81.py
@@ -2,36 +2,32 @@
 from math import factorial
 N = int(input())
 line = list(map(int, input().split()))
-num_list = [str(i) for i in range(N + 1)]
+num_list = [i for i in range(1, N + 1)]
+events = [factorial(N - i) for i in range(N)]
 
-find_index = -1
-find_num = -1
+
+def find_index(index):
+    used = [False] * N
+    result = []
+    digit = N - 1
+    while index > events[digit]:
+        digit -= 1
+
+    for i in range(digit):
+        result.append(num_list[i])
+        used[num_list[i]] = True
+
+    return result
+
+
 if line[0] == 1:
-    find_index = line[1]
+    result = find_index(line[1])
+
 else:
     find_num = ''
     for i in range(1, len(line)):
         find_num += str(line[i])
     find_num = int(find_num)
 
-count = 0
-possible = factorial(N - 1)
-i = 1
-while i < N + 1:
-    num = str(num_list[i])
-    used = [False] * (N + 1)
-    j = (i + 1) % (N + 1)
-    while len(num) < N + 1:
-        if not used[j]:
-            num += str(num_list[j])
-        j = (j + 1) % (N + 1)
-
-    count += 1
-    if count == find_index:
-        print()
-    if find_num == int(num):
-        print(count)
-
-    if count % possible == 0:
-        continue
-    i += 1
+for e in result:
+    print(e)
