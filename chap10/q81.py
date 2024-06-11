91cbf6bef119a3a563902914f3a1f05882b949c3
JIYOUNG KIM <kandallee38@gmail.com>
2024-06-11 22:36:07 +0900

    solving q81 in chap 9

--- chap10/q81.py
+++ chap10/q81.py
@@ -1,32 +1,53 @@
 # BOJ 1722
 from math import factorial
+
 N = int(input())
 line = list(map(int, input().split()))
 num_list = [i for i in range(1, N + 1)]
 events = [factorial(N - i) for i in range(N)]
 
 
-def find_index(index):
-    used = [False] * (N + 1)
+def find_by_index(index):
     result = []
-    digit = N - 1
-    while index > events[digit]:
-        digit -= 1
+    digit = 0
 
-    for i in range(digit + 1):
-        result.append(num_list[i])
-        used[num_list[i]] = True
+    while len(result) < N:
+        count = 0
+        while index > events[digit] * count:
+            count += 1
 
-    return result
+        num = num_list.pop(count - 1)
+        result.append(num)
+        index = index - (events[digit] * (count - 1))
+        digit += 1
+
+    print(*result)
+
+
+def find_by_num(num):
+    result = 1
+    for i in range(len(num)):
+        if num[i] != num_list[0]:
+            count = 0
+            index = 0
+            for j in range(len(num_list)):
+                if num[i] > num_list[j]:
+                    count += 1
+                elif num_list[j] == num[i]:
+                    index = j
+                    break
+
+            result += count + 1
+            num_list.pop(index)
+        else:
+            num_list.pop(0)
+
+    print(result)
 
 
 if line[0] == 1:
-    result = find_index(line[1])
+    find_by_index(line[1])
 
 else:
-    find_num = ''
-    for i in range(1, len(line)):
-        find_num += str(line[i])
-    find_num = int(find_num)
-
-print(*result)
+    find_num = line[1:]
+    find_by_num(find_num)
