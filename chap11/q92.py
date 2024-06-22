1bd634da1dca9e58dbe8648607856c2f5c7bb456
JIYOUNG KIM <kandallee38@gmail.com>
2024-06-22 19:28:36 +0900

    solving q92 in chap 11

--- chap11/q92.py
+++ chap11/q92.py
@@ -1,5 +1,6 @@
 # BOJ 1328
 import sys
+from math import factorial
 
 input = sys.stdin.readline
 N, L, R = map(int, input().split())
@@ -17,12 +18,25 @@ D[2][1][2] = 1
 
 for n in range(3, N + 1):
     # L = 1, R = 1은 N이 2 이상 부터 불가능
-    for l in range(2, n + 1):
-        for r in range(2, l + 1):
+    for l in range(1, n + 1):
+        for r in range(1, l + 1):
             # 좌 우에서 본 건물 수의 합이 전체 건물 수 + 2 이상 배치 불가능
             if l + r < n + 2:
-                D[n][l][r] = (D[n - 1][l - 1][r] + 1) % DIVISOR
+                # if l == r:
+                #     D[n][l][r] = (D[n - 1][l - 1][r] + D[n - 1][l][r - 1]) % DIVISOR
+                # else:
+                #     D[n][l][r] = (D[n - 1][l - 1][r - 1] * factorial(n - 2)) % DIVISOR
+                result = 0
+                for i in range(l + 1):
+                    for j in range(r + 1):
+                        if i < n and j < n:
+                            result += D[n - 1][i][j]
+                D[n][l][r] = result
                 # 좌우 건물의 수를 뒤집었을 때 같은 경우의 수가 나옴
                 D[n][r][l] = D[n][l][r]
 
+# for i in range(1, N + 1):
+#     print("n:", i)
+#     for j in range(len(D[i])):
+#         print(D[i][j])
 print(D[N][L][R] % DIVISOR)
