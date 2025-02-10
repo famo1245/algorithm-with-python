ae03f856d1a4a708f8525ea12fad056fc42bfdbe
JIYOUNG KIM <kandallee38@gmail.com>
2025-02-10 21:56:33 +0900

    ADD: Solved BOJ 11284
    Solved BOJ 11284 using unicode

---
+++ else/BOJ11284.py
@@ -0,0 +1,20 @@
+# BOJ 11284
+import sys
+input = sys.stdin.readline
+
+start = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+middle = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+end = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ',
+       'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+base = ord('가')
+char = ord(input().strip()) - base
+
+end_index = char % 28
+char = char // 28
+middle_index = char % 21
+start_index = char // 21
+
+print(start[start_index])
+print(middle[middle_index])
+print(end[end_index])
